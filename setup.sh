f60a469751333cfffc3ebbe61c8c8160af27ad98
Patrick Sonnerat <patrick.sonnerat@hotmail.com>
2023-09-09 18:59:06 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+gcloud projects create nightscout-vps-xdrip --name="Nightscout Google Cloud"
+
+SERVICE_ACCOUNT_EMAIL=$(gcloud auth list --format="value(account)")
+
+        
 terraform init
 terraform apply -auto-approve
