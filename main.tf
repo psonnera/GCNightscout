e29b405ee4e7903de65f5e37bb3fda14ee28ab66
Patrick Sonnerat <patrick.sonnerat@hotmail.com>
2023-09-09 10:33:59 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,61 @@
+# This code is compatible with Terraform 4.25.0 and versions that are backwards compatible to 4.25.0.
+# For information about validating this Terraform code, see https://developer.hashicorp.com/terraform/tutorials/gcp-get-started/google-cloud-platform-build#format-and-validate-the-configuration
+
+resource "google_compute_instance" "instance-1" {
+  boot_disk {
+    auto_delete = true
+    device_name = "instance-1"
+
+    initialize_params {
+# Need to replace the image with a custom one or fetch automatically the latest version
+      image = "projects/ubuntu-os-cloud/global/images/ubuntu-minimal-2004-focal-v20230901"
+      size  = 30
+      type  = "pd-standard"
+    }
+
+    mode = "READ_WRITE"
+  }
+
+  can_ip_forward      = false
+  deletion_protection = false
+  enable_display      = false
+
+  labels = {
+    goog-ec-src = "vm_add-tf"
+  }
+
+  machine_type = "e2-micro"
+  name         = "instance-1"
+
+  network_interface {
+    access_config {
+      network_tier = "PREMIUM"
+    }
+
+# Need to replace the project name by something genrated automatically and allow region customization
+    subnetwork = "projects/<PROJECTNAME>/regions/us-central1/subnetworks/default"
+  }
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+    preemptible         = false
+    provisioning_model  = "STANDARD"
+  }
+
+# Need to fetch the current service account
+  service_account {
+    email  = "<SERVICEACCOUNTMAIL>.gserviceaccount.com"
+    scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
+  }
+
+  shielded_instance_config {
+    enable_integrity_monitoring = true
+    enable_secure_boot          = false
+    enable_vtpm                 = true
+  }
+
+  tags = ["http-server", "https-server"]
+# Same than above for region customization
+  zone = "us-central1-a"
+}
