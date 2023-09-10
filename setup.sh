01a7645c402e35906f79a1cb5e1c5406b42f8d1e
Patrick Sonnerat <patrick.sonnerat@hotmail.com>
2023-09-10 11:57:18 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,9 @@ SERVICE_ACCOUNT_EMAIL=$(gcloud auth list --format="value(account)")
 # The end user should decide on the VPS region a text interface might be better
 region="us-central1"
 
+# Let's replace default values
+sed -i "s/REGION/$region/g" cloudshell.yaml
+
 # We can then initialize and apply, Terraform will use all present .tf anf tf.json files
 terraform init
 terraform apply -auto-approve
